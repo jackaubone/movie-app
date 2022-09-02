5dfca7df514e5f1ac2748eb475657fb5b6b0ba70
Jack Aubone <jfaubone@gmail.com>
2022-09-02 01:02:51 -0500

    frontend working

--- frontend.rb
+++ frontend.rb
@@ -1,7 +1,34 @@
 require "http"
+quit = false
+p "hi"
 
-movie_response = HTTP.get("http://localhost:3000/movies")
 
-movie_data = movie_response.parse(:json)
+while !quit
 
-puts movie_data
+  puts "[m] for Movies, [a] for Actors"
+  user_input = gets.chomp
+
+  if user_input == "m" || user_input == "a"
+    quit = true
+  else
+    p "Invalid"
+  end
+end
+
+
+if user_input == "m"
+  movie_response = HTTP.get("http://localhost:3000/movies")
+  movie_data = movie_response.parse(:json)
+  movie_data.each do |data|
+    puts "#{data["title"]}, #{data["year"]}, Plot: #{data["plot"]}"
+  end
+else
+  actor_response = HTTP.get("http://localhost:3000/actors")
+  actor_data = actor_response.parse(:json)
+  actor_data.each do |data|
+    puts "#{data["first_name"]} #{data["last_name"]}, known for: #{data["known_for"]}."
+  end
+end
+
+
+
